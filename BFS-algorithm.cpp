69bfe50bf3386ba3e424b209a1b0765d605039c3
Kazi Golam Mahtab <143117451+Mahtab0204@users.noreply.github.com>
2025-05-13 22:12:58 +0600

    Add files via upload

---
+++ BFS-algorithm.cpp
@@ -0,0 +1,61 @@
+/*   n = 10 (nodes), edges = 13
+Edges:
+1 2
+1 4
+2 3
+4 3
+3 9
+3 10
+2 5
+2 7
+2 8
+5 6
+5 7
+7 8
+5 8     */
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>Adj[100];
+vector<bool>visited;
+void BFS(int root)
+{
+    queue<int>q;
+    visited[root]=true;
+    q.push(root);
+
+    while(!q.empty())
+    {
+        int curr=q.front();
+        q.pop();
+        cout<<curr<<" ";
+        for(int x: Adj[curr])
+        {
+            if(!visited[x])
+            {
+                q.push(x);
+                visited[x]=true;
+            }
+        }
+    }
+}
+int main()
+{
+    int n,edges;
+    cin>>n>>edges;
+
+    visited.resize(n+1,false);
+    for(int i=1;i<=edges;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        Adj[u].push_back(v);
+        Adj[v].push_back(u);
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(!visited[i])
+        {
+            BFS(i);
+        }
+    }
+}
